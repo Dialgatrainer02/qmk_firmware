4f6affa4174755c2d8f3bd20cba111dd0c9611a1
Dialgatrainer02 <charliebarker2007@gmail.com>
2024-01-07 14:14:30 +0000

    add personal keymap
    fix compilation errors
    
    dynamic turbo buttons and keymaps chamges
    
    update rgb colour
    
    dynamic macro fixes
    
    oneshot indicator

---
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/Dialgatrainer02/keymap.c
@@ -0,0 +1,189 @@
+/* Copyright 2021 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "features/mouse_turbo_click.h"
+#include "features/sentence_case.h"
+
+// clang-format off
+enum layers{
+  LNX_BASE,
+  LNX_FN,
+  LNX_MO,
+  GAME,
+};
+
+enum custom_keycodes {
+  BRACES = SAFE_RANGE,
+  DBLCLK,
+  TURBO,
+  TURBO_UP,
+  TURBO_DN,
+  USRNAME,
+  LOCK,
+  BASE,
+};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[LNX_BASE] = LAYOUT_ansi_87( 
+     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             TT(LNX_MO),LOCK,     RGB_MOD,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,    KC_HOME,  TG(GAME),
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     BRACES,   BRACES,   KC_BSLS,  KC_DEL,    KC_END,   KC_PGDN,
+     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,              
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,             KC_UP,    
+     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RGUI, MO(LNX_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+[LNX_FN] = LAYOUT_ansi_87( 
+     KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,            KC_TRNS,  KC_TRNS,  RGB_TOG,
+     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,           
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,   
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS),
+     
+[LNX_MO] = LAYOUT_ansi_87( 
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     EE_CLR,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            DM_PLY1,           
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            TURBO,   
+     AC_TOGG,  KC_TRNS,  USRNAME,                                KC_TRNS,                                QK_REP,   DM_REC1,  KC_TRNS,  DM_RSTP,  TURBO_DN, DBLCLK,   TURBO_UP),
+[GAME] = LAYOUT_ansi_87( 
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  BASE,     KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  DM_REC1,  KC_TRNS,  DM_REC2,
+     KC_TRNS,  KC_TRNS,  KC_W,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  DM_PLY1,  DM_RSTP,  DM_PLY2,
+     KC_TRNS,  KC_A,     KC_S,  KC_D,        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,           
+     OSM(MOD_LSFT),      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            TURBO,   
+ OSM(MOD_LCTL), KC_TRNS, OSM(MOD_LALT ),                         KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TURBO_DN, DBLCLK,   TURBO_UP),
+
+
+};
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+  if (!process_mouse_turbo_click(keycode, record, TURBO)) { return false; }
+  if (!process_sentence_case(keycode, record)) { return false; }
+  
+  const uint8_t mods = get_mods();
+  const uint8_t oneshot_mods = get_oneshot_mods();
+  
+  switch (keycode) {
+    case DBLCLK:  // Double click the left mouse button.
+    if (record->event.pressed) {
+      SEND_STRING(SS_TAP(X_BTN1) SS_DELAY(50) SS_TAP(X_BTN1));
+    }
+    return false;
+    case TURBO_UP:  // increase turbo speed
+    if (record->event.pressed) {
+      //click speed up means period is lower
+      extern int MOUSE_TURBO_CLICK_PERIOD; // bring in the varible from ther header
+      MOUSE_TURBO_CLICK_PERIOD -= 5;//mOuse turbo click period is defined in  mouse_turbo_click.c which isnt sourced however the header is
+      if (MOUSE_TURBO_CLICK_PERIOD <= 15) {
+          MOUSE_TURBO_CLICK_PERIOD = 20;
+      }
+    }
+    return false;
+    case TURBO_DN:  // decrease turbo speed
+    if (record->event.pressed) {
+      //click speed down means period is higher
+      extern int MOUSE_TURBO_CLICK_PERIOD; // bring in the varible from the header
+      MOUSE_TURBO_CLICK_PERIOD += 5; 
+    }
+    return false;
+    
+    case BRACES:  // Types [], {}, or <> and puts cursor between braces.
+      if (record->event.pressed) {
+        clear_oneshot_mods();  // Temporarily disable mods.
+        unregister_mods(MOD_MASK_CSAG);
+        if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
+          SEND_STRING("{}");
+        } else if ((mods | oneshot_mods) & MOD_MASK_CTRL) {
+          SEND_STRING("<>");
+        } else {
+          SEND_STRING("[]");
+        }
+        tap_code(KC_LEFT);  // Move cursor between braces.
+        register_mods(mods);  // Restore mods.
+      }
+      return false;
+    case USRNAME:  // send my username
+      if (record->event.pressed) {
+        SEND_STRING("Dialgatrainer069");
+      }
+      return false;
+      
+    case LOCK:  // super l combo
+    if (record->event.pressed) {
+      clear_oneshot_mods();  // Temporarily disable mods.
+      unregister_mods(MOD_MASK_CSAG);
+      SEND_STRING(SS_LGUI("l"));
+      register_mods(mods);
+    }
+    return false;
+    case BASE:
+    if (record->event.pressed) {
+      layer_move(LNX_BASE);
+    }
+    return false;
+  }
+  return true;
+}
+//make bound keys ona higher layer orange
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (get_highest_layer(layer_state) > 0) {
+        uint8_t layer = get_highest_layer(layer_state);
+
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+
+                if (index >= led_min && index < led_max && index != NO_LED &&
+                keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                    rgb_matrix_set_color(index, RGB_ORANGE);
+                }
+            }
+        }
+    }
+  const uint8_t oneshot_mods = get_oneshot_mods();
+  if (oneshot_mods & MOD_MASK_CTRL) {
+    rgb_matrix_set_color(76,RGB_RED);
+  } else if (oneshot_mods & MOD_MASK_SHIFT) {
+    rgb_matrix_set_color(63,RGB_RED);
+  } else if (oneshot_mods & MOD_MASK_ALT) {
+    rgb_matrix_set_color(78,RGB_RED);
+  }  
+  return false;
+}
+
+// disable things that could interfrer with the game
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case GAME:
+        //disable things
+        autocorrect_toggle();
+        autoshift_toggle();
+        caps_word_off();
+        break;
+    default:
+       autocorrect_toggle();
+       autoshift_toggle();
+       caps_word_on(); 
+       clear_oneshot_mods();
+    }
+  return state;
+}
+
+
+
---
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/Dialgatrainer02/rules.mk
@@ -0,0 +1,20 @@
+VIA_ENABLE = yes
+
+AUTO_SHIFT_ENABLE = yes
+AUTOCORRECT_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+
+DYNAMIC_MACRO_ENABLE = yes
+
+SRC += features/sentence_case.c
+
+SRC += features/mouse_turbo_click.c \
+        features/sentence_case.c
+MOUSEKEY_ENABLE = yes
+DEFERRED_EXEC_ENABLE = yes
+
+OPT_DEFS += -DDYNAMIC_KEYMAP_LAYER_COUNT=6
+
+LTO_ENABLE = yes
