b00905fbd61111dbef13937d94ce7d4012851df4
Dialgatrainer02 <oliviabarker900@gmail.com>
2024-06-08 10:01:29 +0000

    begin rewrite of keymap

---
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/Dialgatrainer_rewrite/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+//rgb stuff
+#define ENABLE_RGB_MATRIX_RIVERFLOW//have to define riverflow first as keychron didnt
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_RIVERFLOW
+#define RGB_MATRIX_DEFAULT_HUE 191 //purple
+
+
+//optimisations
+#define NO_MUSIC_MODE
+
+
+//configuring features
+#define DYNAMIC_MACRO_NO_NESTING
+#define MOUSEKEY_INERTIA
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+#define TAPPING_TOGGLE 2
+#define ONESHOT_TIMEOUT 5000
+
+#define SENTENCE_CASE_TIMEOUT 2000
---
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/Dialgatrainer_rewrite/rules.mk
@@ -0,0 +1,17 @@
+#optimisations
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESCAPE_ENABLE = no
+MUSIC_ENABLE = no
+
+
+#features
+DEFERRED_EXEC_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+VIA_ENABLE = yes
+
+SRC += features/sentence_case.c
